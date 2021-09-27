efed1aeb85db8b87dd5a9e89c8b990176347d61b
satou0677905 <83400954+satou0677905@users.noreply.github.com>
2021-09-27 22:33:39 +0900

    Delete js directory

--- js/script2.js
+++
@@ -1,8 +0,0 @@
-//new LuminousGallery(document.querySelectorAll(".grid-gallary"));
-new LuminousGallery(document.querySelectorAll('.grid-gallary'),{},{
-  caption:function(trigger){
-    return trigger.querySelector('img').getAttribute('alt');
-  }
-});
-
-AOS.init({duration:2500});
